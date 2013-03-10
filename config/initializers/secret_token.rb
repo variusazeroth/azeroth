f1db80ca7dc92edff0b60c266521afd021faca2a
Keith Bussey <varius@gmail.com>
2013-03-09 22:50:52 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Azeroth::Application.config.secret_token = 'c5226475f3d5a0edab47ce3bc8df7df1320027874aaee250036d1edf2b4bc05f3fabb58cb29fe8da840e59bc2763fdb75ed46563172d4299812279ce2955f9a9'
